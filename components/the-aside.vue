d9167ecdb0db139c6917b3c629f5e74146c73481
Marco Busellato <marco.busellato@develondigital.com>
2022-07-05 17:59:05 +0200

    aside WIP

---
+++ components/the-aside.vue
@@ -0,0 +1,3 @@
+<template>
+  <aside v-bind="$attrs" class="fixed top-0 left-0 w-[50px] h-screen p-5 bg-white"></aside>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="w-full h-screen overflow-hidden bg-light_green">
-    <the-header />
+    <the-header ref="header" />
+    <the-aside ref="aside" />
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
 import TheHeader from '@/components/the-header.vue'
+import TheAside from '@/components/the-aside.vue'
 
 defineNuxtComponent({
-  TheHeader
+  TheHeader,
+  TheAside
 })
 </script>
