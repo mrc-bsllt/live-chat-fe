bfa988a30de74f7ec795995c10e2fc3120bc3e09
Marco Busellato <marco.busellato@develondigital.com>
2022-07-07 09:19:00 +0200

    remove unocss - add icon component

---
+++ components/commons/icon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg v-bind="$attrs" class="ico" :class="`icon-${name}`">
+    <use :xlink:href="`/ico-defs.svg?v=20210108#icon-${name}`"></use>
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: { type: String, required: true }
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,7 @@
 import { defineNuxtConfig } from 'nuxt'
-import presetIcons from '@unocss/preset-icons'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/scss/tailwind.css", "@/assets/scss/main.scss"],
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  buildModules: ['@unocss/nuxt'],
-  unocss: {
-    preflight: false,
-    presets: [
-      presetIcons()
-    ],
-  }
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt']
 })
