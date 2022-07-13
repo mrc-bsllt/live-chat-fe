5bce72e62dcf6578b2a849dddf4e7467eff43a79
Marco Busellato <marco.busellato@develondigital.com>
2022-07-13 12:37:51 +0200

    add send_request function

--- components/commons/users-list.vue
+++ components/commons/users-list.vue
@@ -1,18 +1,27 @@
 <template>
   <ul v-bind="$attrs" class="w-full h-full">
     <li 
-      v-for="(user, index) in props.list" 
+      v-for="(friend, index) in props.list" 
       :key="index" 
       class="flex flex-row justify-start items-center flex-nowrap py-4" 
       :class="{ 'border-b border-solid border-black': index < props.list.length - 1 }"
     >
-      <thumbnail-avatar :image_path="user.image_path" href="/" :username="user.username" class="text-black" />
-      <button class="btn btn-success ml-10">{{ setLabel(user._id as string) }}</button>
+      <thumbnail-avatar :image_path="friend.image_path" href="/" :username="friend.username" class="text-black" />
+      <button 
+        class="btn ml-10" 
+        :class="setLabel(friend._id as string) !== 'Rimuovi' ? 'btn-success' : 'btn-error'"
+        @click="sendRequest(friend._id as string)"
+        :disabled="setLabel(friend._id as string) === 'In attesa'"
+      >
+          {{ setLabel(friend._id as string) }}
+      </button>
+      <button v-if="setLabel(friend._id as string) === 'Accetta'" class="btn btn-error ml-5">Rifiuta</button>
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
+import { API_HOST } from '@/utils/config'
 import { useUser } from '@/store/user'
 import ThumbnailAvatar from '@/components/commons/thumbnail-avatar.vue'
 import { User } from '@/types/user'
@@ -30,24 +39,43 @@ const props = defineProps({
 
 // Set button label
 const { get_user } = toRefs(useUser())
-const { friends, requests_sent, notifications } = get_user.value
 
 function setLabel(friend_id: string): string {
-  const friend_index = friends?.findIndex(friend => friend._id === friend_id)!
-  const request_index = requests_sent?.findIndex(friend => friend._id === friend_id)!
-  const notification_index = notifications?.findIndex(notification => {
+  const friend_index = get_user.value.friends?.findIndex(friend => friend._id === friend_id)!
+  const request_index = get_user.value.requests_sent?.findIndex(friend => friend._id === friend_id)!
+  const notification_index = get_user.value.notifications?.findIndex(notification => {
     return notification.friend._id === friend_id && notification.text === 'Ti ha inviato una richiesta di amicizia.'
   })!
   
   if(friend_index < 0 && request_index < 0 && notification_index < 0) {
     return 'Aggiungi'
-  } else if(friend_index > 0) {
+  } else if(friend_index >= 0) {
     return 'Rimuovi'
-  } else if(request_index > 0) {
+  } else if(request_index >= 0) {
     return 'In attesa'
   } else {
     return 'Accetta'
   }
+}
+// -------------------------------------------------------------------------
+
+// Submit requests
+const { toggle_refresh_user } = useUser()
+async function sendRequest(friend_id: string) {
+  const token = useCookie('token').value
+
+  await $fetch(`${API_HOST}/api/send-request`, {
+    headers: {
+      Authorization: 'Bearer ' + token
+    },
+    method: 'POST',
+    body: { friend_id },
+    async onResponse({ response }) {
+      if(response.status === 201) {
+        toggle_refresh_user()
+      }
+    }
+  })
 }
 // -------------------------------------------------------------------------
 </script>
